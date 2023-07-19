b7615bf68149590514d051a23cac0c9e1a16608c
somchi29 <chisomkelly70@gmail.com>
2023-07-19 01:19:20 +0100

    Java code

---
+++ src/Seven_segments.java
@@ -0,0 +1,49 @@
+import java.util.Objects;
+
+public class Seven_segments {
+    public static void main(String[] args) {
+        String[][] score = new String[][]{{"|", "|", "|", "|"}, {"|", "|", "|", "|"}, {"|", "|", "|", "|"}, {"|", "|", "|", "|"}, {"|", "|", "|", "|"}};
+        for (int strings = 0; strings < score.length; strings++) {
+            for (int i = 1; i < score.length; i++) {
+                int j;
+                for (j = 0; j < score.length; j++) {
+                    int k;
+                    for (k = 1; k < score.length; k++) {
+
+
+                    }
+                    if (score[j][i].equals("x")) score[j][i] = " ";
+                    System.out.print(score[j][j] + "  ");
+                }
+                System.out.println();
+
+            }
+
+        }
+    }
+}
+
+
+
+//import java.util.Scanner;
+//
+//public class Seven_segments {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        int n = scan.nextInt();
+//        switch (n) {
+//            case 0:
+//                System.out.println("-\n  || \n  ||");
+//
+//
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
